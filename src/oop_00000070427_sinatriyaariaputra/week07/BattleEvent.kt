41b1bc260c8e202172a86b6890cdfed05a1e0d95
Aria <sinatriyaariasap@yahoo.com>
2026-03-26 05:27:22 +0700

    week07: create singleton DatabaseManager

--- src/oop_00000070427_sinatriyaariaputra/week07/BattleEvent.kt
+++
@@ -1,8 +0,0 @@
-﻿package oop_00000070427_sinatriyaariaputra.week07
-
-sealed class BattleState {
-    data class MonsterEncounter(val monsterName: String) : BattleState()
-    data class LootDropped(val item: GameItem) : BattleState()
-    data class GameOver(val reason: String) : BattleState()
-    object SafeZone : BattleState()
-}
--- src/oop_00000070427_sinatriyaariaputra/week07/DatabaseManager.kt
+++ src/oop_00000070427_sinatriyaariaputra/week07/DatabaseManager.kt
@@ -2,6 +2,7 @@
 
 object DatabaseManager {
     var connectionStatus: String = "Disconnected"
+
     fun connect() {
         connectionStatus = "Connected to Server"
         println("Database is ready.")
