ae2ffcad2913e403767e881c57de7c2cdcc00767
Aria <sinatriyaariasap@yahoo.com>
2026-03-26 05:17:18 +0700

    week07: (task) test GameManager singleton in main

--- src/oop_00000070427_sinatriyaariaputra/week07/Main.kt
+++ src/oop_00000070427_sinatriyaariaputra/week07/Main.kt
@@ -29,11 +29,15 @@ fun main() {
 
     println("\n=== TEST SEALED CLASS ===")
     val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-
     val uiMessage = when (response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
         ApiResponse.Loading -> "Tampilkan Spinner"
     }
     println(uiMessage)
+
+    println("\n========== RPG GAME SIMULATION ==========")
+    println("\n--- TEST GAME MANAGER SINGLETON ---")
+    GameManager.startGame()
+    GameManager.startGame()
 }
