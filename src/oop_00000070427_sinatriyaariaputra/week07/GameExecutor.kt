d5a97c9edaf6a2b7671a3055dd33ca2cc2b7469b
Aria <sinatriyaariasap@yahoo.com>
2026-03-26 05:27:27 +0700

    week07: (task) implement exhaustive processEvent

---
+++ src/oop_00000070427_sinatriyaariaputra/week07/GameExecutor.kt
@@ -0,0 +1,10 @@
+﻿package oop_00000070427_sinatriyaariaputra.week07
+
+fun processEvent(event: BattleState) {
+    when (event) {
+        is BattleState.MonsterEncounter -> println("⚔️  Waspada! Monster muncul: ${event.monsterName}. Bersiap untuk bertarung!")
+        is BattleState.LootDropped -> println("🎁 Loot didapat: ${event.item.name} | Damage: ${event.item.damage} | Rarity: ${event.item.rarity} (Drop Chance: ${event.item.rarity.dropChance}%)")
+        is BattleState.GameOver -> println("💀 Game Over! Alasan: ${event.reason}")
+        BattleState.SafeZone -> println("✅ Kamu berada di Safe Zone. Aman untuk beristirahat.")
+    }
+}
