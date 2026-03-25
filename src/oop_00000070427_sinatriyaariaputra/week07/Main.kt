b332324f892eb4d9018d2f4687687ca4d68f1d09
Aria <sinatriyaariasap@yahoo.com>
2026-03-25 15:24:14 +0700

    week07: (trial) trigger private constructor error on NetworkClient

---
+++ src/oop_00000070427_sinatriyaariaputra/week07/Main.kt
@@ -0,0 +1,6 @@
+﻿package oop_00000070427_sinatriyaariaputra.week07
+
+fun main() {
+    // INTENTIONAL COMPILER ERROR – private constructor
+    // val client = NetworkClient("https://api.umn.ac.id")
+}
