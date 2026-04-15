7b4832123496a94f68e0bc79e15180ae22bc1ecc
Aria <sinatriyaariasap@yahoo.com>
2026-04-15 15:22:38 +0700

    week08: simulate unit test failure using !!

--- src/oop_00000070427_sinatriyaariaputra/week08/Main.kt
+++ src/oop_00000070427_sinatriyaariaputra/week08/Main.kt
@@ -61,7 +61,9 @@ fun main() {
 
     println("\n=== TEST JAVA INTEROP ===")
     val javaResponse = LegacyJavaAPI.fetchServerStatus()
-    // Kita menekan tombol !! karena KITA TAHU implementasi Java-nya aman
     val statusLength = javaResponse!!.length
     println("Status dari Java: javaResponse (Length: statusLength)")
+
+    // Unit Test simulation
+    runMockUnitTest()
 }
