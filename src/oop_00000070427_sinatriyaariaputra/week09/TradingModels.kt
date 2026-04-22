46e52cf10c3ddadee5edc720f9c503b2ce90e75c
Aria <sinatriyaariasap@yahoo.com>
2026-04-22 11:07:36 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000070427_sinatriyaariaputra/week09/TradingModels.kt
@@ -0,0 +1,12 @@
+﻿package oop_00000070427_sinatriyaariaputra.week09
+
+// Domain Model untuk Tugas Mandiri Crypto Trading Dashboard
+// roe = Return on Equity dalam persentase
+// status = "OPEN" atau "CLOSED"
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
