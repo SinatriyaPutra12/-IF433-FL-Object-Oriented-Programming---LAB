bcd7dbb021aa914d38076d4ce9f973c30ff297ee
Aria <sinatriyaariasap@yahoo.com>
2026-04-29 13:31:48 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000070427_sinatriyaariaputra/week10/WalletRepository.kt
@@ -0,0 +1,17 @@
+﻿package oop_00000070427_SinatriyaAriaPutra.week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items.toList()
+    }
+
+    fun search(predicate: (T) -> Boolean): List<T> {
+        return items.filter(predicate)
+    }
+}
