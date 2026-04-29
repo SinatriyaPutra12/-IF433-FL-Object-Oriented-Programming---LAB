71bd2cfb454f71153f1b29f1a07c62145832623c
Aria <sinatriyaariasap@yahoo.com>
2026-04-29 13:31:45 +0700

    week10: implement basic generic function printData

---
+++ src/oop_00000070427_sinatriyaariaputra/week10/GenericMethods.kt
@@ -0,0 +1,13 @@
+﻿package oop_00000070427_SinatriyaAriaPutra.week10
+
+fun <T> printData(data: T) {
+    println("Data yang diterima: $data")
+}
+
+fun <T> processData(input: T): T {
+    return input
+}
+
+fun <T> getMax(a: T, b: T): T where T : Comparable<T> {
+    return if (a > b) a else b
+}
