2e7f2cc307604b5cb9122a739ae82155a8a0bebb
Aria <sinatriyaariasap@yahoo.com>
2026-05-06 13:26:14 +0700

    week11: create string extension functions

---
+++ src/oop_00000070427_sinatriyaariaputra/week11/StringExtensions.kt
@@ -0,0 +1,16 @@
+﻿package oop_00000070427_sinatriyaariaputra.week11
+
+// Extension function dengan receiver 'this'
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+// Extension function dengan parameter
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
+
+// Nullable receiver: 'this' bisa bernilai null, jadi harus ditangani
+fun String?.isNullOrEmptyCustom(): Boolean {
+    return this == null || this.isEmpty()
+}
