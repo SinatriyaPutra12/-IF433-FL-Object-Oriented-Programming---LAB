7b63866cffcd9c1a37a874906ad2ca92b232a465
Aria <sinatriyaariasap@yahoo.com>
2026-05-06 13:26:15 +0700

    week11: implement let function for null safety

---
+++ src/oop_00000070427_sinatriyaariaputra/week11/ScopeLetRun.kt
@@ -0,0 +1,20 @@
+﻿package oop_00000070427_sinatriyaariaputra.week11
+
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    // Mengeksekusi blok hanya jika objek tidak null
+    val length = name?.let {
+        println("Nama terdeteksi: $it") // 'it' merujuk pada name
+        it.length // Mengembalikan nilai expression terakhir
+    }
+    println("Panjang nama: $length")
+
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}") // 'this' bisa dihilangkan (implicit)
+        length * 2
+    }
+    println("Hasil kalkulasi run: $result")
+}
