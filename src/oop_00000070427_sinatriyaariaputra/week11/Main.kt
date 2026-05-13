234e4fed0affffbe7f91af39c1cdbc7b7129ed63
Aria <sinatriyaariasap@yahoo.com>
2026-05-13 12:29:48 +0700

    week11: test extension functions in main

--- src/oop_00000070427_sinatriyaariaputra/week11/Main.kt
+++ src/oop_00000070427_sinatriyaariaputra/week11/Main.kt
@@ -3,7 +3,7 @@
 fun main() {
     println("=== TEST EXTENSION FUNCTIONS ===")
     println("Alex".addGreeting())
-    println("Hi".repeatTimes(3))
+    println("Hi".repeatTimes(n = 3))
 
     val text: String? = null
     println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
