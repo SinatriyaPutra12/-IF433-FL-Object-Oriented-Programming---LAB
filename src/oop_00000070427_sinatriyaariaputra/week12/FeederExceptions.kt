1fdcea3f22695315fee2007f8335173144b1fbd1
Aria <sinatriyaariasap@yahoo.com>
2026-05-13 12:29:57 +0700

    week12: (task) create FeederException sealed class

---
+++ src/oop_00000070427_sinatriyaariaputra/week12/FeederExceptions.kt
@@ -0,0 +1,10 @@
+﻿package oop_00000070427_sinatriyaariaputra.week12
+
+// Sealed class sebagai hierarchy exception untuk Smart Feeder
+sealed class FeederException(msg: String) : Exception(msg)
+
+class FoodEmptyException(requested: Int, available: Int) :
+    FeederException("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
+
+class DispenserJamException :
+    FeederException("Wadah dispenser tersangkut/macet!")
