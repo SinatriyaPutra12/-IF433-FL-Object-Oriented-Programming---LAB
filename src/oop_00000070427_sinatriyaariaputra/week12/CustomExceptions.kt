7c26c440b64baff0fca2915cbdcdd7aee9c618d2
Aria <sinatriyaariasap@yahoo.com>
2026-05-13 12:29:55 +0700

    week12: create InsufficientFunds exception

---
+++ src/oop_00000070427_sinatriyaariaputra/week12/CustomExceptions.kt
@@ -0,0 +1,46 @@
+﻿package oop_00000070427_sinatriyaariaputra.week12
+
+// Custom exception dengan membawa data tambahan
+class InsufficientFundsException(
+    val amount: Double,
+    val balance: Double
+) : Exception("Attempted $amount, balance: $balance")
+
+class BankAccount(var balance: Double) {
+    fun withdraw(amount: Double) {
+        if (amount < 0) {
+            throw IllegalArgumentException("Amount must be positive")
+        }
+        if (amount > balance) {
+            throw InsufficientFundsException(amount, balance)
+        }
+        balance -= amount
+        println("Withdrawal successful. Remaining balance: $balance")
+    }
+}
+
+fun main() {
+    println("=== TEST MULTIPLE CATCH ===")
+    val account = BankAccount(100.0)
+
+    try {
+        account.withdraw(150.0) // Ini akan memicu InsufficientFundsException
+    } catch (e: InsufficientFundsException) {
+        println("Caught Domain Error: Uang tidak cukup. ${e.message}")
+    } catch (e: IllegalArgumentException) {
+        println("Caught Argument Error: Input tidak valid. ${e.message}")
+    } catch (e: Exception) {
+        println("Caught General Error: Terjadi kesalahan tidak terduga.")
+    }
+
+    println("\n=== TEST NEGATIVE AMOUNT ===")
+    try {
+        account.withdraw(-50.0)
+    } catch (e: InsufficientFundsException) {
+        println("Caught Domain Error: ${e.message}")
+    } catch (e: IllegalArgumentException) {
+        println("Caught Argument Error: Input tidak valid. ${e.message}")
+    } catch (e: Exception) {
+        println("Caught General Error: Terjadi kesalahan tidak terduga.")
+    }
+}
