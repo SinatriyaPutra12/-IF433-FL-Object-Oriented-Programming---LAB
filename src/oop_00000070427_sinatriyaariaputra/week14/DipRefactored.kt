67cff998fe22cdf884540cc2005d39232d8b9088
Aria <sinatriyaariasap@yahoo.com>
2026-05-25 13:29:25 +0700

    week14: create Database abstraction and implementations

---
+++ src/oop_00000070427_sinatriyaariaputra/week14/DipRefactored.kt
@@ -0,0 +1,17 @@
+﻿package oop_00000070427_sinatriyaariaputra.week14
+
+interface Database {
+    fun query(sql: String): List<String>
+}
+
+class PostgresDatabase : Database {
+    override fun query(sql: String) = listOf("pg_data1", "pg_data2")
+}
+
+class MongoDatabase : Database {
+    override fun query(sql: String) = listOf("mongo_data1", "mongo_data2")
+}
+
+class SafeUserService(private val db: Database) {
+    fun getUser(id: Int) = db.query("SELECT * FROM users WHERE id=$id")
+}
