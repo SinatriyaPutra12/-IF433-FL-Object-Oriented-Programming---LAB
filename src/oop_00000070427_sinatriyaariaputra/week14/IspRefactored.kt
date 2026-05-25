66c867f9cdb6e91f829ec0bf6a5b7b1aa6e47188
Aria <sinatriyaariasap@yahoo.com>
2026-05-25 13:29:24 +0700

    week14: segregate device interfaces

---
+++ src/oop_00000070427_sinatriyaariaputra/week14/IspRefactored.kt
@@ -0,0 +1,23 @@
+﻿package oop_00000070427_sinatriyaariaputra.week14
+
+interface Printable {
+    fun print(doc: String)
+}
+
+interface Scannable {
+    fun scan(doc: String): String
+}
+
+interface Faxable {
+    fun fax(doc: String)
+}
+
+class ModernSimplePrinter : Printable {
+    override fun print(doc: String) = println("Printing securely: $doc")
+}
+
+class AllInOnePrinter : Printable, Scannable, Faxable {
+    override fun print(doc: String) = println("Printing: $doc")
+    override fun scan(doc: String) = "Scanned: $doc"
+    override fun fax(doc: String) = println("Faxing: $doc")
+}
