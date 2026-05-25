0f0ba84ede0b2d2691ed27007c21cce15f73e1aa
Aria <sinatriyaariasap@yahoo.com>
2026-05-25 13:29:23 +0700

    week14: create Shape interface

---
+++ src/oop_00000070427_sinatriyaariaputra/week14/LspRefactored.kt
@@ -0,0 +1,13 @@
+﻿package oop_00000070427_sinatriyaariaputra.week14
+
+interface Shape {
+    fun area(): Int
+}
+
+class SafeRectangle(var width: Int, var height: Int) : Shape {
+    override fun area() = width * height
+}
+
+class SafeSquare(var side: Int) : Shape {
+    override fun area() = side * side
+}
