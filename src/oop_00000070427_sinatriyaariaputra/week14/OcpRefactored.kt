ed3178149f7d1ffa6c1014b823e74babe53f067f
Aria <sinatriyaariasap@yahoo.com>
2026-05-25 13:29:22 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_00000070427_sinatriyaariaputra/week14/OcpRefactored.kt
@@ -0,0 +1,26 @@
+﻿package oop_00000070427_sinatriyaariaputra.week14
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.80
+}
+
+class MemberDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.85
+}
+
+class EmployeeDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.70
+}
+
+// Diskon baru ditambah tanpa menyentuh SafeDiscountCalculator sama sekali
+class SeniorDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.75
+}
+
+class SafeDiscountCalculator(private val strategy: DiscountStrategy) {
+    fun calculate(price: Double) = strategy.apply(price)
+}
